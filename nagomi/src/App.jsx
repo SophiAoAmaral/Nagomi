8422c08d1067e28d346eaf56f6e1aac61808bd45
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-06-04 21:42:44 -0300

    rotas

--- nagomi/src/App.jsx
+++ nagomi/src/App.jsx
@@ -1,9 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router'
+import { Header } from './components/Header'
+import { Sobre } from './components/Sobre'
+
 function App() {
 
 
   return (
     <>
-      
+          <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Header/>}/>
+            <Route path="sobre" element={<Sobre/>}/>
+          </Routes>
+          
+          </BrowserRouter>
             
     </>
   )
--- nagomi/src/components/Header.jsx
+++ nagomi/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
+import {Link} from 'react-router'
 
 export const Header = () => {
   return (
     <header>
-        
+        <nav>
+                <Link to='/'>AAAAAAA</Link>
+                <Link to='sobre'>qwqwq</Link>
+          
+        </nav>
     </header>
   )
 }
