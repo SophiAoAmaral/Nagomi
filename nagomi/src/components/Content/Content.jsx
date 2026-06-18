87bd760262e888d7725ac6adcd1f3c455e2aae6b
sophia <sophiaoliveira2706@outlook.com>
2026-06-18 13:05:51 -0300

    omakase media

--- nagomi/src/components/Content/Content.jsx
+++ nagomi/src/components/Content/Content.jsx
@@ -5,7 +5,7 @@ import { Apresenta } from '../Apresentacao/Apresenta'
 import { Cardapio } from '../Cardapio/Cardapio'
 import { Chef } from '../Chef/Chef'
 import { FormReserva } from '../FormReserva/FormReserva'
-import { Omakase } from '../Reserva/Omakase'
+import { Omakase } from '../Omakase/Omakase'
 import { Footer } from '../Footer/Footer'
 
 export const Content = () => {
